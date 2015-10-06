0f8223fe18b21366625e5d348f2e80b3eaea00ec
Yaismel Miranda <yaismel.miranda@gmail.com>
2015-10-06 15:32:47 -0400

    removed composer_install script

--- build/composer_install.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-PARENT_DIR=$(dirname $(cd "$(dirname "$0")"; pwd))
-
-if [ ! -d "$PARENT_DIR/vendor/bin" ]; then
-    cd "${PARENT_DIR}"
-    php -r "readfile('https://getcomposer.org/installer');" | php
-    php "$PARENT_DIR/composer.phar" install --prefer-source
-fi
